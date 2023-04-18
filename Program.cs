8aeb16263ec80cdf9f24974ed74d9dfca7ce534a
Ilmir Khisamov <khisamchick@yandex.ru>
2023-04-18 21:21:06 +0500

    Homework2

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1
+
+int Random =  new Random().Next(100, 1000);
+
+int number1 = Random % 100;
+int number2 = number1 / 10;
+
+System.Console.WriteLine($"Случайное число: {Random}");
+System.Console.WriteLine($"Вторая цифра: {number2}");
+
+
+//Задача 2
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
+
+
+//Задача 3
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
